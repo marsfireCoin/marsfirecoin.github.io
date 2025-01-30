9b92b4dfa18a90d55d337fb39d7614cf719ac3a6
marsfireCoin <marsfireorg@gmail.com>
2025-01-30 17:10:05 +0530

    changes

--- game.js
+++ game.js
@@ -1,102 +1,78 @@
-// game.js - Updated Version
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+// game.js
 
-canvas.width = 800;
-canvas.height = 500;
+let gameStarted = false;
+let playerTokens = 0;
+let web3;
+let userAccount;
 
-const spaceship = { x: 400, y: 400, width: 40, height: 40, speed: 5 };
-const coins = [];
-const asteroids = [];
-let score = 0;
-
-document.addEventListener("keydown", moveSpaceship);
-
-function moveSpaceship(event) {
-    if (event.key === "ArrowLeft" && spaceship.x > 0) spaceship.x -= spaceship.speed;
-    if (event.key === "ArrowRight" && spaceship.x < canvas.width - spaceship.width) spaceship.x += spaceship.speed;
-    if (event.key === "ArrowUp" && spaceship.y > 0) spaceship.y -= spaceship.speed;
-    if (event.key === "ArrowDown" && spaceship.y < canvas.height - spaceship.height) spaceship.y += spaceship.speed;
+// Web3 Setup
+if (typeof window.ethereum !== 'undefined') {
+  web3 = new Web3(window.ethereum);
+} else {
+  alert("Please install MetaMask to play the game!");
 }
 
-function spawnCoin() {
-    coins.push({ x: Math.random() * (canvas.width - 20), y: Math.random() * (canvas.height - 20), size: 20 });
-}
-
-function spawnAsteroid() {
-    asteroids.push({ x: Math.random() * canvas.width, y: 0, size: 30, speed: Math.random() * 3 + 2 });
-}
-
-function drawSpaceship() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(spaceship.x, spaceship.y, spaceship.width, spaceship.height);
-}
-
-function drawCoins() {
-    ctx.fillStyle = "gold";
-    coins.forEach((coin, index) => {
-        ctx.beginPath();
-        ctx.arc(coin.x, coin.y, coin.size / 2, 0, Math.PI * 2);
-        ctx.fill();
-        if (detectCollision(spaceship, coin)) {
-            coins.splice(index, 1);
-            score += 10;
-            spawnCoin();
-        }
-    });
-}
-
-function drawAsteroids() {
-    ctx.fillStyle = "gray";
-    asteroids.forEach((asteroid, index) => {
-        ctx.beginPath();
-        ctx.arc(asteroid.x, asteroid.y, asteroid.size / 2, 0, Math.PI * 2);
-        ctx.fill();
-        asteroid.y += asteroid.speed;
-        if (asteroid.y > canvas.height) asteroids.splice(index, 1);
-        if (detectCollision(spaceship, asteroid)) gameOver();
-    });
-}
-
-function detectCollision(obj1, obj2) {
-    return obj1.x < obj2.x + obj2.size &&
-           obj1.x + obj1.width > obj2.x &&
-           obj1.y < obj2.y + obj2.size &&
-           obj1.y + obj1.height > obj2.y;
-}
-
-function gameOver() {
-    alert("Game Over! Score: " + score);
-    document.location.reload();
-}
-
-async function rewardPlayer() {
-    if (!signer || !userAddress) {
-        alert("Connect Wallet First!");
-        return;
-    }
-
-    const contract = new ethers.Contract(contractAddress, contractABI, signer);
-    const amount = ethers.utils.parseUnits("10", 18);
-
+// Function to connect wallet
+async function connectWallet() {
+  if (typeof window.ethereum !== 'undefined') {
     try {
-        const tx = await contract.transfer(userAddress, amount);
-        await tx.wait();
-        alert("10 MarsFireCoin Sent to Your Wallet!");
-    } catch (error) {
-        console.error("Transaction failed", error);
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      userAccount = accounts[0];
+      alert('Connected to ' + userAccount);
+    } catch (err) {
+      alert("Connection failed. Please try again.");
     }
+  }
 }
 
-function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSpaceship();
-    drawCoins();
-    drawAsteroids();
-    if (score >= 50) rewardPlayer();
-    requestAnimationFrame(update);
+// Start the game
+function startGame() {
+  if (!gameStarted) {
+    gameStarted = true;
+    document.getElementById("gameStatus").innerText = "Game started! Collect MarsFireCoin tokens!";
+    document.querySelector(".game-start-button").disabled = true;
+    setupGameBoard();
+    connectWallet(); // Connect wallet at the start of the game
+  }
 }
 
-spawnCoin();
-setInterval(spawnAsteroid, 2000);
-update();
+// Function to set up the game board
+function setupGameBoard() {
+  let gameBoard = document.getElementById("gameBoard");
+  gameBoard.innerHTML = ''; // Clear the board
+
+  // Example of a simple game: Collecting MarsFireCoin tokens
+  let tokenButton = document.createElement("button");
+  tokenButton.innerText = "Collect MarsFireCoin";
+  tokenButton.classList.add("collect-button");
+  tokenButton.onclick = collectToken;
+
+  gameBoard.appendChild(tokenButton);
+}
+
+// Function to handle token collection
+function collectToken() {
+  if (!userAccount) {
+    alert("Please connect your wallet first!");
+    return;
+  }
+  playerTokens++;
+  document.getElementById("gameStatus").innerText = "You collected " + playerTokens + " MarsFireCoin tokens!";
+  rewardPlayer(playerTokens);
+}
+
+// Function to reward player with MarsFireCoin
+async function rewardPlayer(tokens) {
+  // Web3 transaction for rewarding tokens (example)
+  // This should be integrated with the MarsFireCoin smart contract
+  alert(`Rewarding ${tokens} MarsFireCoin tokens to ${userAccount}`);
+  
+  // Example Web3 call to transfer tokens
+  // You can replace this with the actual smart contract interaction
+  const tx = {
+    from: userAccount,
+    to: "0xYourSmartContractAddress", // Replace with MarsFireCoin contract address
+    value: web3.utils.toWei("0.1", "ether"), // Example reward (replace with MarsFireCoin value)
+  };
+  await web3.eth.sendTransaction(tx);
+}
