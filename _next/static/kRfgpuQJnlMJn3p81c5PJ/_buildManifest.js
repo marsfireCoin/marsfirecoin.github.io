b62c080654fdbb4763854b37170e5b1e248ec08f
marsfireCoin <marsfireorg@gmail.com>
2025-02-02 18:54:57 +0530

    Updates

---
+++ _next/static/kRfgpuQJnlMJn3p81c5PJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/927-d8ed53ed782d0d83.js","static/chunks/pages/index-ff3ff9f50de18074.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
